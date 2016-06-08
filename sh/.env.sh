101055721fe9814caef2e578f3dc4cf89b7c5bde
zhouyixing <1505134838@qq.com>
2016-06-08 22:42:11 +0800

    1. Set man syntax highlighting

--- sh/.env.sh
+++ sh/.env.sh
@@ -1,3 +1,11 @@
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m' 
+
 export PREFIX_PATH=${HOME}/Self
 if [ -d ${PREFIX_PATH} ] ; then
     export PATH=${PREFIX_PATH}/bin:${PATH}
